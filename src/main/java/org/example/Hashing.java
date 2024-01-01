c324d40dbfd936b8c2014a33cfb038caff8c62c7
Kai Wang <69681670+honeyland414@users.noreply.github.com>
2023-12-31 23:56:14 -0500

    test brance merge (#1)

--- src/main/java/org/example/Hashing.java
+++ src/main/java/org/example/Hashing.java
@@ -1,4 +1,21 @@
 package org.example;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class Hashing {
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                res[0] = i;
+                res[1] = map.get(target - nums[i]);
+                return res;
+            }
+            map.put(nums[i], i);
+        }
+        return res;
+    }
 }
