d29e40cfc5c03c989f92af4d3a605d083e2a4397
honeyland414 <wangkai.honeyland@gmail.com>
2023-12-31 23:47:28 -0500

    test brance merge

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,14 @@
 package org.example;
 
 
+import java.util.Arrays;
+
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        Hashing hashing = new Hashing();
+
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        System.out.println(Arrays.toString(hashing.twoSum(nums, target)));
     }
 }
